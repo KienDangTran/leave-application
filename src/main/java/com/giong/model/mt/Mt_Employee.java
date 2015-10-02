624f9255f847b04ef5ee688df2090c6f76d8ab80
KienDangTran <kiendang91@gmail.com>
2015-10-02 14:16:22 +0700

    Done config

--- src/main/java/com/giong/model/mt/Mt_Employee.java
+++ src/main/java/com/giong/model/mt/Mt_Employee.java
@@ -2,9 +2,12 @@ package com.giong.model.mt;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -12,62 +15,73 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "MT_EMPLOYEE")
 public class Mt_Employee {
-
+	
 	@Id
 	@Column(name = "EMPLOYEE_CODE", unique = true, nullable = false)
 	private String employee_code;
-
+	
 	public String getEmployee_code() {
-		return employee_code;
+		return this.employee_code;
 	}
-
+	
 	public void setEmployee_code(String employee_code) {
 		this.employee_code = employee_code;
 	}
-
+	
 	@Column(name = "EMPLOYEE_NAME", nullable = false)
 	private String employee_name;
-
+	
 	public String getEmployee_name() {
-		return employee_name;
+		return this.employee_name;
 	}
-
+	
 	public void setEmployee_name(String employee_name) {
 		this.employee_name = employee_name;
 	}
-
+	
 	@Temporal(TemporalType.DATE)
 	@Column(name = "DATE_OF_BIRTH")
 	private Date date_of_birth;
-
+	
 	public Date getDate_of_birth() {
-		return date_of_birth;
+		return this.date_of_birth;
 	}
-
+	
 	public void setDate_of_birth(Date date_of_birth) {
 		this.date_of_birth = date_of_birth;
 	}
-
+	
 	@Column(name = "EMAIL")
 	private String email;
-
+	
 	public String getEmail() {
-		return email;
+		return this.email;
 	}
-
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	
 	@Column(name = "PHONE_NO")
 	private String phone_no;
-
+	
 	public String getPhone_no() {
-		return phone_no;
+		return this.phone_no;
 	}
-
+	
 	public void setPhone_no(String phone_no) {
 		this.phone_no = phone_no;
 	}
-
+	
+	@OneToOne(mappedBy = "mt_employee", cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+	private Mt_User mt_User;
+	
+	public Mt_User getMt_User() {
+		return this.mt_User;
+	}
+	
+	public void setMt_User(Mt_User mt_User) {
+		this.mt_User = mt_User;
+	}
+	
 }
--- src/main/java/com/giong/model/mt/Mt_User.java
+++ src/main/java/com/giong/model/mt/Mt_User.java
@@ -5,70 +5,70 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "MT_USER")
 public class Mt_User {
-
+	
 	@Id
 	@Column(name = "USER_ID", unique = true, nullable = false)
 	private int user_id;
-
+	
 	public int getUser_id() {
-		return user_id;
+		return this.user_id;
 	}
-
+	
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
-
+	
 	@Column(name = "USERNAME", unique = true, nullable = false)
 	private String username;
-
+	
 	public String getUsername() {
-		return username;
+		return this.username;
 	}
-
+	
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
+	
 	@Column(name = "PASSWORD", nullable = false)
 	private String password;
-
+	
 	public String getPassword() {
-		return password;
+		return this.password;
 	}
-
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
 	@Column(name = "EMPLOYEE_CODE", unique = true, nullable = false)
 	private String employee_code;
-
+	
 	public String getEmployee_code() {
-		return employee_code;
+		return this.employee_code;
 	}
-
+	
 	public void setEmployee_code(String employee_code) {
 		this.employee_code = employee_code;
 	}
-
-	@OneToOne(mappedBy = "employee_code", cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
-	@PrimaryKeyJoinColumn
+	
+	@OneToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+	@JoinColumn(name = "employee_code")
 	private Mt_Employee mt_employee;
-
+	
 	public Mt_Employee getMt_employee() {
-		return mt_employee;
+		return this.mt_employee;
 	}
-
+	
 	public void setMt_employee(Mt_Employee mt_employee) {
 		this.mt_employee = mt_employee;
 		this.employee_code = mt_employee == null ? null : mt_employee.getEmployee_code();
 	}
-
+	
 }

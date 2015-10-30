84d064722113ac241702c6c70153324c5a36383e
KienDangTran <kiendang91@gmail.com>
2015-10-30 10:18:46 +0700

    Update config 7

--- src/main/java/com/giong/managedbean/LoginManagedBean.java
+++ src/main/java/com/giong/managedbean/LoginManagedBean.java
@@ -3,18 +3,17 @@ package com.giong.managedbean;
 import java.io.IOException;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
-import org.springframework.security.web.WebAttributes;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.giong.util.FacesUtil;
 
@@ -24,38 +23,53 @@ public class LoginManagedBean extends AbtractManagedBean {
 	
 	private static final long serialVersionUID = 1L;
 	
+	private String username;
+	private String password;
 	
-	public void doLogin() throws ServletException, IOException {
+	@ManagedProperty(value = "#{authenticationManager}")
+	private AuthenticationManager authenticationManager;
+	
+	public String doLogin() throws ServletException, IOException {
 		
-		final ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		final RequestDispatcher dispatcher = ((ServletRequest) context.getRequest()).getRequestDispatcher("/login");
-		dispatcher.forward((ServletRequest) context.getRequest(), (ServletResponse) context.getResponse());
-		FacesContext.getCurrentInstance().responseComplete();
-		
-		final Exception e = (Exception) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(WebAttributes.AUTHENTICATION_EXCEPTION);
-		if (e instanceof BadCredentialsException) {
-			e.printStackTrace();
-			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(WebAttributes.AUTHENTICATION_EXCEPTION, null);
-			FacesUtil.sendFatalMessage(this.getLocalizedMessage("error.user_pass_is_invalid"), "");
-			//			this.err = this.getLocalizedMessage("error.user_pass_is_invalid");
+		try {
+			final Authentication request = new UsernamePasswordAuthenticationToken(this.getUsername(), this.getPassword());
+			final Authentication result = this.authenticationManager.authenticate(request);
+			SecurityContextHolder.getContext().setAuthentication(result);
+			return "success";
 		}
-		else if (e instanceof LockedException || e instanceof DisabledException) {
+		catch (final LockedException | DisabledException e) {
 			e.printStackTrace();
-			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(WebAttributes.AUTHENTICATION_EXCEPTION, null);
-			FacesUtil.sendErrorMessage(this.getLocalizedMessage("error.user_acc_has_been_suspended"), "");
-			//			this.err = this.getLocalizedMessage("error.user_acc_has_been_suspended");
+			FacesUtil.sendErrorMessage(this.getLocalizedMessage("error.user_acc_has_been_suspended", this.getUsername()), "");
 		}
-		
+		catch (final BadCredentialsException e) {
+			e.printStackTrace();
+			FacesUtil.sendErrorMessage(this.getLocalizedMessage("error.user_pass_is_invalid"), "");
+		}
+		return "failure";
 	}
 	
-	//	private String err;
-	//	
-	//	public String getErr() {
-	//		return this.err;
-	//	}
-	//	
-	//	public void setErr(String err) {
-	//		this.err = err;
-	//	}
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public AuthenticationManager getAuthenticationManager() {
+		return this.authenticationManager;
+	}
+	
+	public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
 	
 }
